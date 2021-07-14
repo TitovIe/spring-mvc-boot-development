293424838c452ab60c60cbb05b01d524a7c537fd
Ilya <titov.ie@phystech.edu>
2021-07-15 01:46:06 +0300

    Add test

---
+++ work-application/src/test/java/com/db/client/OperationTest.java
@@ -0,0 +1,19 @@
+package com.db.client;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringJUnitWebConfig(classes = com.db.client.config.Config.class)
+public class OperationTest {
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+
+}
